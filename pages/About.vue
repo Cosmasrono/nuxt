4724ff77744dacc57b505baf47837d3866913eb8
cosmas <cosmas@localhost.localdomain>
2022-11-29 20:56:53 +0300

    start

---
+++ pages/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>about</main>
+  <div class=" caret-lime-600">hello world</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class=" min-h-screen bg-slate-800 text-white">Home</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
