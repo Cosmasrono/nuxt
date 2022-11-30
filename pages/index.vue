1cb2fcd17c488d3b2ffc4dd8605e777cfc290d6e
cosmas <cosmas@localhost.localdomain>
2022-11-30 18:27:01 +0300

    changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,72 @@
 <template>
-  <main class="bg-black">
-      <!--   <aside class="top-1 justify-start h-full md:mt-1 md:w-1/5 md:left md:ml-2 md:fixed md:min-h-screen md:flex" aria-label="Sidebar"> -->
-  <div class="overflow-y-auto py-2 rounded">
-   <!-- <div class="flex flex-col gap-y-2 justify-center items-center mx-auto"> -->
- <ul class="flex flex-col gap-4 justify-start ml-1 text-white">
-    <li><Nuxtlink to="/home">Home</Nuxtlink></li>
- </ul>
+  <main class="bg-black">  
+   <div class="flex flex-row gap-10 justify-end items-center pr-40">
+   <!--  <img src="../assets/linkedn.png" alt="" class=""> 
+<img src="../assets/facebook.png" alt="" class=""> -->
   </div>
-        <!-- </aside> -->
- <div class="flex justify-center items-center text-xl text-white">
+  <div class="flex flex-col md:flex-row">
+<aside class="mt-5 ml-10 w-64" aria-label="Sidebar">
+   <div class="overflow-y-auto px-3 py-4 bg-gray-50 rounded dark:bg-gray-800">
+      <ul class="space-y-2">
+        <li>
+            <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+               <svg aria-hidden="true" class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
+               <span class="ml-3 text-4xl hover:text-green-500">Home</span>
+            </a>
+         </li>
+         <li>
+            <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+               <svg aria-hidden="true" class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
+               <span class="ml-3 hover:text-green-500">Dashboard</span>
+            </a>
+         </li>
+          
+         <li> 
+            <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+               <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path><path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path></svg>
+               <span class="flex-1 ml-3 whitespace-nowrap hover:text-green-500">Inbox</span>
+          </a>
+         </li>
+         <li>
+            <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+               <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+               <span class="flex-1 ml-3 whitespace-nowrap hover:text-green-500">Users</span>
+            </a>
+         </li>
+         <li>
+            <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+               <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd"></path></svg>
+               <span class="flex-1 ml-3 whitespace-nowrap hover:text-green-500">Products</span>
+            </a>
+         </li>
+          </ul>
+        
+     
+      <div id="dropdown-cta" class="p-4 mt-6 bg-blue-50 rounded-lg dark:bg-blue-900" role="alert">
+         <div class="flex items-center mb-3">
+              
+         </div>
+       
+   </div>
+   </div>
+</aside>
+ 
+<p class="pt-24 pl-10 text-white"><span class="text-lg text-green-500 md:text-3xl">Trust us for better tomorrow </span><br>
+    Nuxt 3 is an open-source framework for web development
+     that is both simple and powerful. Nuxt 3 was developed by Sébastien Chopin to emulate the
+      functionality of Nuxt.js, but with Vue instead of React. Furthermore, it is entirely written in
+       typescript for easy maintenance and built-in typescript support.
+We observe that Nuxt 3 is currently unstable, deeming it unfit to be production-ready.
+ Let take a look at whats available.
+
+</p>
+  </div>
+ 
+        <div class="">
+        <h1 class="pt-20 pb-48 m-6 text-4xl text-white">MEN HERE WE GO DOWN TO LEARN NUXT <span class="text-green-600">THE VUE FRAMEWORK</span> </h1>
+        <button class="p-7 mb-20 ml-10 text-4xl text-white bg-green-500 rounded-lg hover:scale-105">GET STARTED</button>
+        </div>
+ <div class="flex justify-center items-center pb-5 text-xl text-white">
   Hello welcome to this platform feel free and relax
  </div>
  <div class="container grid grid-cols-1 grid-rows-3 gap-4 justify-center items-center mx-auto mb-20 md:grid-rows-2 md:grid-cols-3 lg:grid-cols-3">
@@ -23,20 +81,21 @@
     <img src="../assets/9.webp" alt="" class="w-80 h-80 rounded-md hover:scale-105">
 
  </div>
+ <div class="mt-20">
 
-<label class="block mt-12 mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload file</label>
-<input class="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file">
-
+<label class="pt-14 text-xl font-medium text-white dark:text-white" for="file_input">Upload file</label>
+<input class="block text-sm text-white bg-black rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file">
+</div>
 
 
 <form>
-   <div class="mb-12 w-full rounded border border-gray-200 -lg bg-fullgray-50 dark:bg-gray-700 dark:border-gray-600">
-       <div class="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
+   <div class="w-full bg-fullgray-50 dark:bg-gray-700 dark:border-gray-600">
+       <div class="px-4 py-2 mt-10 bg-white rounded-t-lg dark:bg-gray-800">
            <label for="comment" class="sr-only">Your comment</label>
            <textarea id="comment" rows="4" class="px-0 w-full text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="Write a comment..." required></textarea>
        </div>
        <div class="flex justify-between items-center px-3 py-2 border-t dark:border-gray-600">
-           <button type="submit" class="inline-flex items-center px-4 py-2.5 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
+           <button type="submit" class="inline-flex items-center px-4 py-2.5 text-xs font-medium text-center text-white bg-green-500 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-black">
                Post comment
            </button>
            <div class="flex pl-0 space-x-1 sm:pl-2">
@@ -62,7 +121,12 @@
 
 <script>
 export default {
-  
+  mounted(){
+    this.$nextTick(()=>{
+        this.$nuxt.$loading.start()
+        setTimeout(()=>this.$nuxt.$loading.finish(),500)
+    })
+  }
 
 }
 </script>
